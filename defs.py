de02a0abc971ad2819cb12df208ccecfb8fab297
Vinícius Alves <viniciusmra@gmail.com>
2023-03-20 22:36:53 -0300

    ellemento

--- defs.py
+++ defs.py
@@ -1,4 +1,5 @@
 from skimage.morphology import skeletonize, skeletonize_3d
+from scipy.spatial import distance
 import cv2
 import numpy as np
 
@@ -19,52 +20,52 @@ def binarize(original_image):
 #Retorna todos os vizinhos com a mesma cor de um pixel
 def getNeighbors(img, pixel):
     x, y = pixel
-    level = img[x][y]
+    level = img[y][x]
     neighbors = []
 
     queue = deque([(x, y)])
     visited = numpy.zeros_like(img, dtype=bool)
-    visited[x][y] = True
+    visited[y][x] = True
     
     while len(queue) > 0:
         current_pixel = queue.popleft()
         qx, qy = current_pixel
 
-        if(checkPixel(img, (qx - 1, qy - 1), level) and not visited[qx - 1][qy - 1]):
+        if(checkPixel(img, (qx - 1, qy - 1), level) and not visited[qy - 1][qx - 1]):
             queue.append((qx - 1, qy - 1))
-            visited[qx - 1][qy - 1] = True
+            visited[qy - 1][qx - 1] = True
         
-        if(checkPixel(img, (qx, qy - 1), level) and not visited[qx][qy - 1]):
+        if(checkPixel(img, (qx, qy - 1), level) and not visited[qy - 1][qx]):
             queue.append((qx, qy - 1))
-            visited[qx][qy - 1] = True
+            visited[qy - 1][qx] = True
         
-        if(checkPixel(img, (qx + 1, qy - 1), level) and not visited[qx+1][qy-1]):
+        if(checkPixel(img, (qx + 1, qy - 1), level) and not visited[qy - 1][qx + 1]):
             queue.append((qx + 1, qy - 1))
-            visited[qx+1][qy-1] = True
+            visited[qy - 1][qx + 1] = True
             
-        if(checkPixel(img, (qx - 1, qy), level) and not visited[qx - 1][qy]):
+        if(checkPixel(img, (qx - 1, qy), level) and not visited[qy][qx - 1]):
             queue.append((qx - 1, qy))
-            visited[qx - 1][qy] = True
+            visited[qy][qx - 1] = True
 
-        if(checkPixel(img, (qx + 1, qy), level) and not visited[qx + 1][qy]):
+        if(checkPixel(img, (qx + 1, qy), level) and not visited[qy][qx + 1]):
             queue.append((qx + 1, qy))
-            visited[qx + 1][qy] = True
+            visited[qy][qx + 1] = True
 
-        if(checkPixel(img, (qx - 1, qy + 1), level) and not visited[qx - 1][qy + 1]):
+        if(checkPixel(img, (qx - 1, qy + 1), level) and not visited[qy + 1][qx - 1]):
             queue.append((qx - 1, qy + 1))
-            visited[qx - 1][qy + 1] = True
+            visited[qy + 1][qx - 1] = True
 
-        if(checkPixel(img, (qx, qy + 1), level) and not visited[qx][qy + 1]):
+        if(checkPixel(img, (qx, qy + 1), level) and not visited[qy + 1][qx]):
             queue.append((qx, qy + 1))
-            visited[qx][qy + 1] = True
+            visited[qy + 1][qx] = True
         
-        if(checkPixel(img, (qx + 1, qy + 1), level) and not visited[qx + 1][qy + 1]):
+        if(checkPixel(img, (qx + 1, qy + 1), level) and not visited[qy + 1][qx + 1]):
             queue.append((qx + 1, qy + 1))
-            visited[qx + 1][qy + 1] = True
+            visited[qy + 1][qx + 1] = True
         
         neighbors.append(current_pixel)
         
-        img[qx][qy] = 200
+        #img[qx][qy] = 200
 
     return neighbors
 
@@ -72,13 +73,13 @@ def getNeighbors(img, pixel):
 # checa se um determinado pixel de uma imagem tem valor "level"
 def checkPixel(img, pixel, level):
     x, y = pixel
-    max_x = len(img)
-    max_y = len(img[0])
+    max_y = len(img)
+    max_x = len(img[0])
 
     if(x < 0 or y < 0 or x >= max_x or y >= max_y): #verifica se aquele pixel existe na imagem
         return False
     
-    if(img[x][y] == level):
+    if(img[y][x] == level):
         return True
     else:
         return False
@@ -86,24 +87,28 @@ def checkPixel(img, pixel, level):
 def clearBorder(original_img, color):
     img = original_img.copy()
     groups = []
-    for x in range(0, len(img)):
-        if(img[x][0] == 0):
-            groups.append(getNeighbors(img, (x, 0)))
-
-        if(img[x][len(img[0])-1] == 0):
-            groups.append(getNeighbors(img, (x, len(img[0])-1)))
-    
-    for y in range(0, len(img[0])):
-        if(img[0][y] == 0):
+    for y in range(0, len(img)):
+        if(img[y][0] == 0):
             groups.append(getNeighbors(img, (0, y)))
+            img = colorizeGroup(img, groups[-1], color)
 
-        if(img[len(img)-1][y] == 0):
-            groups.append(getNeighbors(img, (len(img)-1, y)))
+        if(img[y][len(img[0]) - 1] == 0):
+            groups.append(getNeighbors(img, (len(img[0]) - 1, y)))
+            img = colorizeGroup(img, groups[-1], color)
+    
+    for x in range(0, len(img[0])):
+        if(img[0][x] == 0):
+            groups.append(getNeighbors(img, (x, 0)))
+            img = colorizeGroup(img, groups[-1], color)
+
+        if(img[len(img) - 1][x] == 0):
+            groups.append(getNeighbors(img, (x, len(img) - 1)))
+            img = colorizeGroup(img, groups[-1], color)
             
     for group in groups:
         for pixel in group:
             x, y = pixel
-            img[x][y] = color
+            img[y][x] = color
 
     return img
 
@@ -112,7 +117,7 @@ def colorizeGroup(original_image, group, grayscale):
 
     for pixel in group:
             x, y = pixel
-            image[x][y] = grayscale
+            image[y][x] = grayscale #ok
 
     return image
 
@@ -122,9 +127,9 @@ def getGroups(origial_img):
     #new_image = img.copy()
     groups = []
     
-    for x in range(0, len(image)):
-        for y in range(0, len(image[0])):
-            if(image[x][y] == 0):
+    for y in range(0, len(image)):
+        for x in range(0, len(image[0])):
+            if(image[y][x] == 0):
                 groups.append(getNeighbors(image, (x, y)))
                 image = colorizeGroup(image, groups[-1], 1)
 
@@ -152,10 +157,11 @@ def floodFill(original_img):
 def getBounds(groups):
     groupBounds = []
     for group in groups:
-        minY = group[0][1] 
-        maxY = group[0][1] 
         minX = group[0][0]
         maxX = group[0][0] 
+        minY = group[0][1] 
+        maxY = group[0][1] 
+       
         for pixel in group:
             x, y = pixel
             if(x < minX):
@@ -172,20 +178,68 @@ def getBounds(groups):
 
 def createSubImage(group, groupSize):
 
-    print(groupSize)
-
-    width = groupSize[2] - groupSize[0]
-    height = groupSize[3] - groupSize[1]
+    width = groupSize[2] - groupSize[0] + 3
+    height = groupSize[3] - groupSize[1] + 3
 
     minX = groupSize[0]
     minY = groupSize[1]
 
     image = np.ones((height, width), np.uint8) * 255
-    print(len(image))
-    print(len(image[0]))
+
     for pixel in group:
         x, y = pixel
-        #print("(" + str(x-minX) + ", " + str(y - minY) + ")")
-        image[x - minX][y - minY] = 0
-	
+        image[y - minY + 1][x - minX + 1] = 0
+
     return image
+
+def getEccentricity(img):
+    window = np.ones((3, 3), dtype=np.uint8)
+    center = (0,0)
+    tips = []
+    max = 0
+    for i in range(1, len(img[0])):  # x
+        for j in range(2, len(img)): # y
+            if(img[j][i] == 0):
+                window = img[j-1:j+2, i-1:i+2]
+                value = np.sum(np.logical_not(window))
+                if(value > max):
+                    center = (i, j)
+                    max = value
+                if(value == 2):
+                    tips.append((i, j))
+
+    tipsDistance = []
+    
+    for tip in tips:
+        tipsDistance.append(distance.euclidean(center, tip))
+    r_max = np.max(tipsDistance)
+    r_min = np.min(tipsDistance)
+    e = (r_max - r_min) / (r_max + r_min)
+    return e
+    
+    
+         
+# def vertexDistance(img):
+# 	janela = np.ones((5, 5), dtype=np.uint8)
+# 	max1_coords = []
+# 	max2_coords = []
+# 	distancias = []
+# 	count_vizinhos = []
+# 	for i in range(len(img[0])-4):
+# 		for j in range(len(img)-4):
+# 			if(img[j][i] == 0):             
+# 					janela_pixels = img[i:i+5, j:j+5] 
+# 					total_pixels_pretos = np.sum(np.logical_not(janela_pixels))
+# 					coords = np.argwhere(janela_pixels == 0) #verifica se o pixel eh preto
+#             else:
+            
+# 			# if len(coords) > 1:
+# 			# 	coords = coords[np.argsort(coords[:, 0])]
+# 			# 	max1_coords.append([i+coords[0, 0], j+coords[0, 1]])
+# 			# 	max2_coords.append([i+coords[1, 0], j+coords[1, 1]])
+# 			# 	distancia = distance.euclidean(max1_coords[-1], max2_coords[-1])
+
+# 	# Cria a matriz resultante
+# 	#distancia = np.column_stack((np.array(max1_coords), np.array(max2_coords), np.array(distancias)))
+# 	return distancia
+
--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 from PIL import Image
 import cv2
+from skimage.morphology import skeletonize, skeletonize_3d
 
 import defs
 
 if __name__ == '__main__':
-    image = cv2.imread("images/1gear.png", 0)
+    image = cv2.imread("images/teste3.png", 0)
     binary_image = defs.binarize(image)
+    print("binarizou")
     
-    noborder_image = defs.clearBorder(binary_image, 255)
+    noborder_image = defs.clearBorder(binary_image, 240)
+    print("limpou a borda")
 
-    nohole_image = defs.floodFill(noborder_image)
+    #nohole_image = defs.floodFill(noborder_image)
 
-    groups = defs.getGroups(nohole_image)
+    print("Antes da segmentação")
+
+    groups = defs.getGroups(noborder_image)
     
     groupSize = defs.getBounds(groups)
 
-    subimg = defs.createSubImage(groups[0], groupSize[0])
+    print("Antes da excentricidade")
+    
+    for index, group in enumerate(groups):
+        subimg = defs.createSubImage(group, groupSize[index])
+        subimg = defs.floodFill(subimg) # fechamento
+        skeleton = skeletonize(cv2.bitwise_not(subimg))
+        skeleton = cv2.bitwise_not(skeleton.astype('uint8') * 255)
+        e = defs.getEccentricity(skeleton)
+        if(e > 0.2):
+            noborder_image = defs.colorizeGroup(noborder_image, group, 170)
+        print(str(index) + " - " + str(e))
+        
 
-    cv2.imshow('Esqueleto', cv2.resize(subimg, (800, 600)))
+    # skeleton = skeletonize(cv2.bitwise_not(subimg))
+    # skeleton = cv2.bitwise_not(skeleton.astype('uint8') * 255)
+    # print(defs.getEccentricity(skeleton))
+    #skeleton_binary = defs.binarize(skeleton)
+    
+    cv2.imshow('Resultado', cv2.resize(noborder_image, (1000, 500)))
+    #cv2.imwrite('points1.png', skeleton)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-    # #img = numpy.asarray(binarize('gear_hack.png'))
-    # img = numpy.asarray(defs.binarize('engrenagens.png'))
-    # print(img)
-    # img_copy = img.copy()
-    # img_copy = defs.clearBorder(img_copy, 200)
-    # groups = defs.pixelsGroups(img_copy)
-    # print(defs.groupSize(groups))
-    # #for x in range(0, len(img_copy)):
-
-
-
-    # Image.fromarray(img_copy).show()
+    #distance = defs.ellementoestruturanteflamengoganhoudovasco(skeleton) #testar
+    #print(distance)
+    
